463f55c71d354f3e176a02e8d693f18a982dd94a
ai006 <thierry.ishimwe@gmail.com>
2026-08-10 19:51:18 -0500

    cpu-o3: Add speculative store bypass predictor scaffolding
    Adds the initial ssbp class declaration and definition to the O3
    CPU along with its SConscript entry. The predictor is not yet wired
    into the memory dependence unit.

---
+++ src/cpu/o3/ssbp.hh
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2004-2005 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __CPU_O3_SSBP_HH__
+#define __CPU_O3_SSBP_HH__
+
+namespace gem5
+{
+
+namespace o3
+{
+
+
+/**
+ * Implements the reverse engineered AMD's Speculative-Store-Bypass Predictor
+ * found on AMD Zen 3 CPU for determining if memory
+ * instructions are dependent upon each other.  See paper "Uncovering
+ * and Exploiting AMD Speculative Memory Access Predictors for Fun and
+ * Profit" by Lyu and Qiu.
+ * modeled according to the paper by Ange
+ */
+class SSBP : public Named
+{
+    public:
+        /** Default constructor.  init() must be called prior to use. */
+        SSBP() : Named("SSBP") {};
+        /** Default destructor. */
+        ~SSBP();
+
+};
+
+
+} // namespace o3
+
+} // namespace gem5
+
+
+#endif // __CPU_O3_SSBP_HH__
